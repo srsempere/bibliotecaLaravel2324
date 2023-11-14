9884831f4fe98850041f8521614121e0c061897c
Samu <samueljose.garcia@iesdonana.org>
2023-11-14 15:35:47 +0100

    Justo antes de breeze

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ArticuloController;
+use App\Http\Controllers\CategoriaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('articulos', ArticuloController::class);
+Route::resource('categorias', CategoriaController::class);

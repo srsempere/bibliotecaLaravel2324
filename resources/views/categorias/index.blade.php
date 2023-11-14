ca7da8ee673a94270b0856e3d00c267ae48b2292
Samu <samueljose.garcia@iesdonana.org>
2023-11-14 16:18:33 +0100

    Mostrando las cateogorias en una tabla de flowbite

--- resources/views/categorias/index.blade.php
+++ resources/views/categorias/index.blade.php
@@ -1,5 +1,25 @@
 <x-guest-layout>
-    @foreach ($categorias as $categoria)
-        {{ $categoria->name }}
-    @endforeach
+
+
+    <div class="relative overflow-x-auto">
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        Categoría
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($categorias as $categoria)
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <td class="px-6 py-4">
+                            {{ $categoria->name }}
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
 </x-guest-layout>

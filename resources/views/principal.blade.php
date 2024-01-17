a28108fe562a3fb8870eda41833264fe3b0ac28f
Samuel García <samueljose.garcia@iesdonana.org>
2024-01-17 21:01:07 +0100

    Mostrando artículos con sus imágenes en principal

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -4,6 +4,9 @@
             @foreach ($articulos as $articulo)
                 <div
                     class="p-6 max-w-xs min-w-full bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                    <a href="{{ route('articulos.show', ['articulo' => $articulo]) }}">
+                        <img src="{{ asset($articulo->miniatura_url) }}" alt="Imagen del libro.">
+                    </a>
                     <a href="#">
                         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                             {{ $articulo->nombre }}
